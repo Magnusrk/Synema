06edbd1e14308463b234d99204850e31e59ae616
Magnus <magnusrk90@gmail.com>
2023-09-25 11:12:05 +0200

    Added movie list skeleton

---
+++ app/src/main/java/com/example/synema/Data/Datasource.kt
@@ -0,0 +1,14 @@
+package com.example.synema.Data
+
+import com.example.synema.R
+import com.example.synema.model.Movie
+
+
+class Datasource() {
+    fun loadMovies(): List<Movie>{
+        return listOf<Movie>(
+            Movie(R.string.movie1, R.drawable.image1),
+            Movie(R.string.movie2, R.drawable.image2)
+        )
+    }
+}
--- app/src/main/java/com/example/synema/MainActivity.kt
+++ app/src/main/java/com/example/synema/MainActivity.kt
@@ -11,6 +11,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.synema.ui.theme.SynemaTheme
+import com.example.synema.model.Movie
+import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.lazy.items
+import com.example.synema.Data.Datasource
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MoviesApp()
                 }
             }
         }
@@ -43,4 +62,55 @@ fun GreetingPreview() {
     SynemaTheme {
         Greeting("Android")
     }
+}
+
+@Preview
+@Composable
+private fun MovieCardPreview() {
+    MovieCard(Movie(R.string.movie1, R.drawable.image1))
+}
+
+@Preview
+@Composable
+fun MoviesApp() {
+    MovieList(
+        movieList = Datasource().loadMovies(),
+    )
+}
+
+@Composable
+fun MovieList(movieList: List<Movie>, modifier: Modifier = Modifier) {
+    LazyRow(modifier = modifier) {
+        items(movieList) { movie ->
+            MovieCard(
+                movie = movie,
+                modifier = Modifier.padding(8.dp)
+            )
+
+        }
+    }
+}
+
+
+@Composable
+fun MovieCard(movie: Movie, modifier: Modifier = Modifier) {
+    Card (modifier = modifier) {
+        Column {
+            Image(
+                painter = painterResource(movie.imageResourceId),
+                contentDescription = stringResource(movie.stringResourceId),
+                modifier = Modifier
+                    .width(95.dp)
+                    .height(135.dp),
+                contentScale = ContentScale.FillBounds
+            )
+            Text(
+                text = LocalContext.current.getString(movie.stringResourceId),
+                modifier = Modifier.padding(15.dp),
+                style = MaterialTheme.typography.labelMedium
+            )
+        }
+
+    }
+
 }
