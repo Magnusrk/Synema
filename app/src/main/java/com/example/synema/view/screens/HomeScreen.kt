6c131993fbde5ca094db99c2bc74b9e37b4cc1a2
madsvnielsen <mads@hvn.dk>
2023-10-09 12:10:22 +0200

    - Merged!
     _._     _,-'""`-._
    (,-.`._,'(       |\`-/|
        `-.-' \ )-`( , o o)
              `-    \`_`"'-

--- app/src/main/java/com/example/synema/view/screens/HomeScreen.kt
+++ app/src/main/java/com/example/synema/view/screens/HomeScreen.kt
@@ -18,8 +18,10 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -88,7 +90,7 @@ fun MoviesApp() {
 }
 
 @Composable
-fun MovieList(movieList: List<Movie>, modifier: Modifier = Modifier, header: String) {
+fun MovieList(movieList: List<MovieModel>, modifier: Modifier = Modifier, header: String) {
 
     Column(
         modifier = Modifier
@@ -117,7 +119,7 @@ fun MovieList(movieList: List<Movie>, modifier: Modifier = Modifier, header: Str
 
 
 @Composable
-fun MovieCard(movie: Movie, modifier: Modifier = Modifier) {
+fun MovieCard(movie: MovieModel, modifier: Modifier = Modifier) {
     Surface(
         modifier = modifier,
         shape = RoundedCornerShape(10.dp), // Customize the shape if needed
