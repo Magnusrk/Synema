4e6be2a015fd336b5055757239b4a77c903ec118
MathiasW <s224293@student.dtu.dk>
2023-10-30 11:08:52 +0100

    Started making the watchlists screen

--- app/src/main/java/com/example/synema/view/screens/WatchlistScreen.kt
+++ app/src/main/java/com/example/synema/view/screens/WatchlistScreen.kt
@@ -2,17 +2,25 @@ package com.example.synema.view.screens
 
 import GradientBox
 import MoviePosterFrame
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.synema.model.ProfileModel
@@ -21,6 +29,8 @@ import com.example.synema.view.components.MainContainer
 import com.example.synema.view.components.SynemaLogo
 import com.example.synema.view.components.TopBar
 import com.example.synema.view.utils.Size
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 
 
 @Composable
@@ -29,6 +39,7 @@ fun WatchList(navController : NavHostController, profileState: MutableState<Prof
         Column {
             MainContainer(hasBottomNav = true){
                 TopBar(title = "My Watchlists", alignment = Alignment.Center)
+                newWatchlist()
             };
             BottomBar(navController = navController)
         }
@@ -63,3 +74,29 @@ private fun SynHeader() {
         SynemaLogo()
     }
 }
+
+@Composable
+private fun newWatchlist(){
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .height(100.dp)
+        .offset(34.dp, 33.dp)
+    ){
+
+        Box(modifier = Modifier
+            .height(100.dp)
+            .width(100.dp)
+            .background(Color(0xFFB15FA8))){
+            Column(modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Box(modifier = Modifier.size(40.dp,4.dp).background(Color(0xFF63105B)))
+                Box(modifier = Modifier.size(4.dp,40.dp).background(Color(0xFF63105B)))
+
+            }
+        }
+        }
+
+    }
+
