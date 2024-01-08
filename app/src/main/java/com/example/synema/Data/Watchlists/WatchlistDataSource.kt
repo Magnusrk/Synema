228246dc0e4e424019d26c3288d20f7d3355a5dc
MathiasW <s224293@student.dtu.dk>
2024-01-08 12:29:58 +0100

    Making things

--- app/src/main/java/com/example/synema/Data/Watchlists/WatchlistDataSource.kt
+++ app/src/main/java/com/example/synema/Data/Watchlists/WatchlistDataSource.kt
@@ -8,17 +8,32 @@ import com.example.synema.model.WatchlistModel
 interface WatchlistDataSource {
 
 
+    fun createWatchlist(
+        watchlistName: String,
+        token: String,
+        callback: (ApiResponse<MovieModel>) -> Unit
+    )
 
-    fun createWatchlist(watchlistName: String, token : String, callback: (ApiResponse<MovieModel>) -> Unit)
-    fun read_db(token : String, callback: (ApiResponse<List<WatchlistModel>>) -> Unit)
+    fun read_db(token: String, callback: (ApiResponse<List<WatchlistModel>>) -> Unit)
 
     fun addMovieToWatchlist(
         watchlistId: String,
         movieId: String,
-        token : String,
+        token: String,
         callback: (ApiResponse<String>) -> Unit
     )
 
-    fun getWatchlistById(watchlistId: String,token: String, callback: (ApiResponse<WatchlistModel>) -> Unit)
+    fun getWatchlistById(
+        watchlistId: String,
+        token: String,
+        callback: (ApiResponse<WatchlistModel>) -> Unit
+    )
+
+    fun deleteMovieFromWatchlist(
+        watchlistId: String,
+        movieId: String,
+        token: String,
+        callback: (ApiResponse<String>) -> Unit
+    )
 }
 
