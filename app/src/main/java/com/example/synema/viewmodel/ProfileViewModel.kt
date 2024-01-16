d9fbfc3a69f849054a4852be96638a6c217808b7
madsvnielsen <mads@hvn.dk>
2024-01-16 17:30:22 +0100

    Profile view model was overwritten

--- app/src/main/java/com/example/synema/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/synema/viewmodel/ProfileViewModel.kt
@@ -1,12 +1,9 @@
 package com.example.synema.viewmodel
 
 import android.content.Context
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.synema.Data.DataStore.DataStoreManager
@@ -16,31 +13,17 @@ import com.example.synema.model.ProfileModel
 import com.example.synema.model.ReviewModel
 import com.example.synema.model.UserModel
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
-import java.time.Period
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
-import kotlin.time.Duration.Companion.seconds
 
 class ProfileViewModel : ViewModel() {
 
     var context = AppContext.getInstance()
     val source = DependencyProvider.getInstance().getUserSource()
     var followerCount = mutableIntStateOf(0)
-    var followerList = mutableStateListOf<String>()
-    var reviewList = mutableStateListOf<ReviewModel>()
     val profileState = AppContext.getInstance().getProfileState().value
 
-    val movieSource = DependencyProvider.getInstance().getMovieSource()
 
-
-
-    @RequiresApi(Build.VERSION_CODES.O)
     fun init(profileModel: ProfileModel){
-        getListOfFollowers(profileModel)
+        getFollowers(profileModel)
     }
 
 
@@ -59,45 +42,14 @@ class ProfileViewModel : ViewModel() {
         context.getNav().navigate("editprofile")
     }
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun getFollowerReviews(){
-        reviewList.clear()
-        for (user in followerList) {
-            movieSource.getOtherUserReviews(user, profileState.token) {
-                if (it.successful()) {
-                    it.getResult()?.let { reviewModel ->
-                        reviewList.addAll(reviewModel)
-                        sortReviews()
-                    }
-                }
-            }
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun sortReviews(){
-        val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
-        //reviewList.sortWith(Comparator.comparing { a -> LocalDate.parse(a.date, dateTimeFormatter).toEpochDay().seconds.absoluteValue })
-
-        reviewList.sortBy {
-            val from = LocalDateTime.parse(it.date, dateTimeFormatter)
-            from.toInstant(ZoneOffset.UTC).epochSecond
-        }
-        reviewList.reverse()
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun getListOfFollowers(profileModel: ProfileModel){
-        source.getFollowing(profileModel.id, profileState.token) {
+    fun getFollowers(profileModel: ProfileModel) {
+        source.getFollowers(profileModel.id, profileState.token) {
             if (it.successful()) {
-                it.getResult()?.let { profile ->
-                    followerList.clear()
-                    followerList.addAll(profile)
-                    followerCount.value = profile.size
-                    getFollowerReviews()
+                it.getResult()?.let { string ->
+                    followerCount.value = string.size
                 }
             }
         }
-    }
 
+    }
 }
